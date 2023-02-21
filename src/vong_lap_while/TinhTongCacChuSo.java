19fa03d5cf9eb778e6f12d8d5f9bbeb5742fea0d
Tuan Anh <88950574+jkay-kmm@users.noreply.github.com>
2023-02-21 14:01:54 +0700

    bai tap tinh tong cac chu so

---
+++ src/vong_lap_while/TinhTongCacChuSo.java
@@ -0,0 +1,29 @@
+package vong_lap_while;
+ 
+import java.util.Scanner;
+ 
+public class TinhTongCacChuSo {
+ 
+    public static void main(String[] args) {
+        int number, balance;
+        int sum = 0;
+        Scanner scanner = new Scanner(System.in);
+         
+        System.out.println("Input int: ");
+        number = scanner.nextInt();
+         
+        while (number <= 0) {
+            System.out.println("Int must be greater than 0: ");
+            number = scanner.nextInt();
+        }
+         
+        while (number > 0) {
+            balance = number % 10;
+            sum += balance;
+            number /= 10;
+        }
+         
+        System.out.println("the sum character is  = " + sum);
+    }
+ 
+}
